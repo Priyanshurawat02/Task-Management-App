535168fc09c7835e2051bd9f22afdbfc84d9e03b
Priyanshu rawat <priyanshurawat0311@gmail.com>
2026-01-26 15:51:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import TaskManagementApp from './TaskManagement'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <TaskManagementApp/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/TaskManagement.jsx
@@ -0,0 +1,7 @@
+export default function TaskManagementApp(){
+    return (
+        <div>
+            <h1>Task Management</h1>
+        </div>
+    )
+} 
